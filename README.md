3b8ecc61a6a5bff3558ca3197cd8bf50f29d2dd4
Aruna Gamage <admgbit@gmail.com>
2018-01-06 21:47:31 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Event-Bus-Library # http://tutorialwing.com/android-eventbus-library-example/
---
+++ app/src/main/java/com/example/eventbuslibrarytutorial/Events.java
@@ -0,0 +1,42 @@
+package com.example.eventbuslibrarytutorial;
+
+/**
+ * Created by aruna on 1/6/18.
+ */
+
+public class Events {
+
+    // Event used to send message from fragment to activity.
+    public static class FragmentActivityMessage {
+        private String message;
+        public FragmentActivityMessage(String message) {
+            this.message = message;
+        }
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    // Event used to send message from activity to fragment.
+    public static class ActivityFragmentMessage {
+        private String message;
+        public ActivityFragmentMessage(String message) {
+            this.message = message;
+        }
+        public String getMessage() {
+            return message;
+        }
+    }
+
+    // Event used to send message from activity to activity.
+    public static class ActivityActivityMessage {
+        private String message;
+        public ActivityActivityMessage(String message) {
+            this.message = message;
+        }
+        public String getMessage() {
+            return message;
+        }
+    }
+
+}
